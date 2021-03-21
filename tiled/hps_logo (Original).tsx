b114219a0c63c6b329ee5265933d7a874719aec9
666yannis666 <81108132+666yannis666@users.noreply.github.com>
2021-03-21 20:26:17 +0100

    added Else

---
+++ tiled/hps_logo (Original).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="hps_logo (Original)" tilewidth="32" tileheight="32" tilecount="255" columns="15">
+ <image source="C:/Users/Yannis/Downloads/hps_logo (Original).jpg" width="507" height="569"/>
+</tileset>
